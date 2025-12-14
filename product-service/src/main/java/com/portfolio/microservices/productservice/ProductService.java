6a879bf43ec605f15bc2a4f6afdec6709b2ecf0d
stefanobini99 <stefanobini99@gmail.com>
2025-12-14 10:18:42 +0100

    feat(product): implement product service with CRUD

---
+++ product-service/src/main/java/com/portfolio/microservices/productservice/ProductService.java
@@ -0,0 +1,30 @@
+package com.portfolio.microservices.productservice;
+
+import com.portfolio.microservices.productservice.dto.ProductRequest;
+import com.portfolio.microservices.productservice.dto.ProductResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class ProductService {
+
+    private final ProductRepository productRepository;
+
+    public void createProduct(ProductRequest productRequest) {
+        Product product = Product.builder()
+                .name(productRequest.name())
+                .description(productRequest.description())
+                .price(productRequest.price())
+                .build();
+        productRepository.save(product);
+    }
+
+    public List<ProductResponse> getAllProducts() {
+        return productRepository.findAll().stream()
+                .map(product -> new ProductResponse(product.getId(), product.getName(), product.getDescription(), product.getPrice()))
+                .toList();
+    }
+}
---
+++ product-service/src/main/java/com/portfolio/microservices/productservice/dto/ProductResponse.java
@@ -0,0 +1,6 @@
+package com.portfolio.microservices.productservice.dto;
+
+import java.math.BigDecimal;
+
+public record ProductResponse(Long id, String name, String description, BigDecimal price) {
+}
